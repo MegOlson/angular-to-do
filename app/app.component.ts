1c216fd498e48163ed0ba09bc1ce2bf1b65eb3ee
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-23 15:12:54 -0700

    Update to use ngModule.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-
-    <ul>
-      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">
-        {{currentTask.description}}
-        <button (click)="editTask()">Edit!</button>
-      </li>
-    </ul>
-  </div>
+    <div class="container">
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <ul>
+        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+      </ul>
+      <hr>
+      <div>
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1-3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+      </div>
+    </div>
   `
 })
 
@@ -28,9 +37,10 @@ export class AppComponent {
     new Task('Brainstorm group projects', 2),
     new Task('Add READMEs to repos', 2)
   ];
+  selectedTask: Task = this.tasks[0];
 
-  editTask() {
-    alert("You just requested to edit a Task!");
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task) {
