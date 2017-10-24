aaed34f3b1cc45ecdfaf0b13331cfe360c7d586a
Kelsey Langlois and Megan Olson <kelsey@>
2017-10-24 11:27:26 -0700

    Add edit-task component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormsModule }  from '@angular/forms';
 import { TaskListComponent } from './task-list.component';
+import { EditTaskComponent } from './edit-task.component';
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import { TaskListComponent } from './task-list.component';
     FormsModule],
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    EditTaskComponent
   ],
   bootstrap: [ AppComponent ]
 })
