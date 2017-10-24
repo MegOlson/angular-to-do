bf65a157174d6e2089049c9d0a7e2d24f45d3f63
Kelsey Langlois and Megan Olson <kelsey@>
2017-10-24 11:36:28 -0700

    Add new task form and component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -11,6 +11,7 @@ import { Task } from './task.model';
       <hr>
       <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)=
       "finishedEditing()"></edit-task>
+      <new-task (newTaskSender)="addTask($event)"></new-task>
     </div>
   `
 })
@@ -35,5 +36,9 @@ export class AppComponent {
 
   finishedEditing() {
     this.selectedTask = null;
+  }
+
+  addTask(newTaskFromChild: Task) {
+    this.masterTaskList.push(newTaskFromChild);
   }
 }
